384000189148ccf8e9c17104b2d8c31cbee3cd79
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-03-24 13:34:11 +0330

    created Blogs database with mango and mangoose

--- app.js
+++ app.js
@@ -1,44 +1,53 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
+// express app
 const app = express();
 
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://farzaneh:EGh1goiaoNh8GdgD@ninjanode.orhguy6.mongodb.net/ninja-node?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// register view engine
 app.set("view engine", "ejs");
-// app.set("views","myviews")
 
-app.listen(3000);
-
+// middleware & static files
 app.use(express.static("public"));
-
 app.use(morgan("dev"));
 
+// routes
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
+// blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new Blog" });
 });
 
+// 404 page
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
