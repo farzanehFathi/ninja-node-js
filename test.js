8e2390c69bb4f1a70d826a1669adca12092d796c
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-04-02 09:47:13 +0330

    requests and responses

--- test.js
+++ test.js
@@ -1,41 +1,45 @@
-const { execSync } = require("child_process");
+const http = require("http");
 const fs = require("fs");
 
-// fs.readFile("./docs/blog.txt", (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(data.toString());
-// });
+const server = http.createServer((req, res) => {
+  // set header content type
+  res.setHeader("content-Type", "text/html");
 
-// fs.writeFile("./docs/blog3.txt", "Hello, I'm writing this blog", (err) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log("file was written");
-// });
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "./about");
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
 
-// if (!fs.existsSync("./assets")) {
-//   fs.mkdir("./assets", (err) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log("folder created");
-//   });
-// } else {
-//   fs.rmdir("./assets", (err) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log("folder deleted");
-//   });
-// }
-
-if (fs.existsSync("./docs/deleteme.txt")) {
-  fs.unlink("./docs/deleteme.txt", (err) => {
+  //send an html file
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.end(data);
     }
-    console.log("file deleted");
   });
-}
+});
+
+const port = 3000;
+
+server.listen(3000, "localhost", () => {
+  console.log(`listening for requests on port ${port}`);
+});
