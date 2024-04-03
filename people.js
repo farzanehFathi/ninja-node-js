d6c5566771c2618119e164c4b77342306142f0da
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-04-03 19:35:42 +0330

    started learning express

--- people.js
+++
@@ -1,8 +0,0 @@
-const ppl = ["fari", "mari", "nani"];
-const ages = [32, 33, 35];
-
-const dict = (ppl, ages) => {
-  console.log(`${ppl} are ${ages} yo respectively!`);
-};
-
-module.exports = { ppl, ages, dict };
