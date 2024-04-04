b50bcdbb2a9c95704d7caaa4eead8c6fa764ff56
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-04-04 21:06:23 +0330

    html to ejs

--- app.js
+++ app.js
@@ -2,21 +2,24 @@ const express = require("express");
 
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 let port = 3000;
 app.listen(port);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
