43393fa0b379af0ac7193d9be01a487119bcd173
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-04-07 15:44:56 +0330

    mongoose & mongoDB :)

--- app.js
+++ app.js
@@ -1,27 +1,59 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const env = require("dotenv").config().parsed;
+const Blog = require("./models/blog");
 
 const app = express();
+let port = 3000;
+
+//connect to MongoDB
+const dbURI = `mongodb+srv://farzaneh:${env.password}@ninjanode.orhguy6.mongodb.net/ninja-node?retryWrites=true&w=majority`;
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //register view engine
 app.set("view engine", "ejs");
-
-let port = 3000;
-app.listen(port);
 
 //middlware & static files
 app.use(express.static("public"));
 
 // app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  const blogs = [];
+app.get("/single-blog", (req, res) => {
+  Blog.findById("65fff412c0d4481029d3776e")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-  res.render("index", { title: "Home", blogs });
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+// blogs routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
