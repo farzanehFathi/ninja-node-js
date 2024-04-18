ddc1d5fe87fdf5f78532cd90912c97be669ec919
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-04-18 17:50:17 +0330

    delete post

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const app = express();
 
 // register view engine
 app.set("view engine", "ejs");
+
+// middlware
 app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
 
 // connect to mongoDB
 const dbURI = `mongodb+srv://farzaneh:${env.password}@ninjanode.orhguy6.mongodb.net/ninjaNode?retryWrites=true&w=majority`;
@@ -25,29 +28,49 @@ mongoose
 // listen for requests
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
+  res.redirect("/blogs");
   // res.sendFile("./views/index.html", root:{__dirname})
 });
 
-app.get("/blogs", (req, res) => {});
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "This is just a new blog",
-    snippet: "I'm testing myself",
-    body: "I hope to scuced",
-  });
-
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
   blog
     .save()
     .then((result) => {
-      res.send(result);
+      res.redirect("/blogs");
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/blog/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { title: "Blog Details", blog: result });
     })
     .catch((err) => console.log(err));
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About Us" });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => res.json({ redirect: "/blogs" }))
+    .catch((err) => console.log(err));
 });
 
 app.get("/blogs/create", (req, res) => {
