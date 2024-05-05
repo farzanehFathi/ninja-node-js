9466dc76b1ff80e17e22c36a9bc71f1501a061ec
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-05-05 14:04:03 +0330

    Revert "Worked on blog features"
    This reverts commit 3d47fa96d8626264780167c0968e02dc1bbb6a94.

--- app.js
+++ app.js
@@ -2,32 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
-
-const connectDB = require("./server/config/db");
 
 const app = express();
-const PORT = 3001 || process.env.PORT;
-
-// Connect to DB
-connectDB();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser);
-
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-      mongoUrl: process.env.dbURI,
-    }),
-  })
-);
+const PORT = 3000 || process.env.PORT;
 
 app.use(express.static("public"));
 
@@ -37,7 +14,6 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
-app.use("/", require("./server/routes/admin"));
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,81 +1,19 @@
 const express = require("express");
+
 const router = express.Router();
-const Post = require("../models/Post");
 
-// GET
-// HOME
-router.get("", async (req, res) => {
-  try {
-    const locals = {
-      title: "NodeJs Blog",
-      description: "Simpe Blog created with NodeJs, Express & MongoDb",
-    };
+// Routes
+router.get("", (req, res) => {
+  const locals = {
+    title: "NodeJs Blog",
+    description: "Simpe Blog created with NodeJs, Express & MongoDb",
+  };
 
-    let perPage = 7;
-    let page = req.query.page || 1;
-
-    const data = await Post.aggregate([{ $sort: { Createdat: -1 } }])
-      .skip(perPage * page - perPage)
-      .limit(perPage)
-      .exec();
-
-    const count = await Post.countDocuments();
-    const nextPage = parseInt(page) + 1;
-    const hasNextPage = nextPage <= Math.ceil(count / perPage);
-
-    res.render("index", {
-      locals,
-      data,
-      current: page,
-      nextPage: hasNextPage ? nextPage : null,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("An error occurred while fetching posts.");
-  }
+  res.render("index", { locals });
 });
 
-// GET
-// post: id
-router.get("/post/:id", async (req, res) => {
-  try {
-    const slug = req.params.id;
-    const data = await Post.findById({ _id: slug });
-
-    const locals = {
-      title: "NodeJs Blog",
-      description: `${data.title}`,
-    };
-
-    res.render("post", { locals, data });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-// POST
-// post search term
-
-router.post("/search", async (req, res) => {
-  try {
-    const locals = {
-      title: "Search",
-      description: "search for content",
-    };
-
-    let searchTerm = req.body.searchTerm;
-    const searchNoSpcialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "");
-
-    const data = await Post.find({
-      $or: [
-        { title: { $regex: new RegExp(searchNoSpcialChar, "i") } },
-        { body: { $regex: new RegExp(searchNoSpcialChar, "i") } },
-      ],
-    });
-    res.render("search", { locals, data });
-  } catch (err) {
-    console.log(err);
-  }
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 
 module.exports = router;
