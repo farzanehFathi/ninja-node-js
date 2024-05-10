48fb848846f8d89511c4842c9450946888ef65f8
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-05-10 13:28:34 +0330

    pagination created
    + added pagination code in main.js
    + Formatted pagination link in index.ejs

--- server/routes/main.js
+++ server/routes/main.js
@@ -6,14 +6,31 @@ const Post = require("../models/Post");
 
 // GET --- HOME
 router.get("", async (req, res) => {
-  const locals = {
-    title: "NodeJs Blog",
-    description: "Simpe Blog created with NodeJs, Express & MongoDb",
-  };
-
   try {
-    const data = await Post.find();
-    res.render("index", { locals, data });
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simpe Blog created with NodeJs, Express & MongoDb",
+    };
+
+    let perPage = 7;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { CreatedAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    // const data = await Post.find();
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
   } catch (err) {
     console.log(err);
   }
