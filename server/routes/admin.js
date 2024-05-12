cdc7c61e47d3b3efb786e5780658d9a7e75af52c
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-05-12 13:04:21 +0330

    defined user model and login function

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 const adminLayout = "../views/layouts/admin";
 
-//GET --- HOME
+//GET --- Admin - Login Page
 
 router.get("/admin", async (req, res) => {
   try {
@@ -14,6 +15,24 @@ router.get("/admin", async (req, res) => {
     };
 
     res.render("admin/index", { locals, layout: adminLayout });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//POST --- Admin - Check Login
+
+router.post("/admin", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (req.body.username === "admin" && req.body.password === "password") {
+      res.send("you are logged in");
+    } else {
+      res.send("wrong username and password");
+    }
+
+    res.redirect("/admin");
   } catch (err) {
     console.log(err);
   }
